7890ae6e887e32de404012f3f62df3bed1098832
juulkkaa <163435470+juulkkaa@users.noreply.github.com>
2024-04-16 18:51:37 +0200

    wip: add new menu function, add user search

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from models.data_source import users
-from utils.crud import read_friends, add_user
+from utils.crud import read_friends, add_user, search_user
 
-if __name__== '__main__':
+if __name__ == '__main__':
     while True:
         print("Welcome to the menu choose an option: ")
         print("1. Read a list of friends")
         print("0. Exit ")
         print("2. Add new user ")
+        print("3. Search user ")
         menu_option = input("Choose an option: ")
         if menu_option == "0":
             break
         if menu_option == "1":
             read_friends(users)
         if menu_option == "2":
+            add_user(users)
+        if menu_option == "3":
             add_user(users)
